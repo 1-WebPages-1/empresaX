eba8250543109c2179686c97506b04528063bf88
1-WebPages-1 <cabraljorge683@gmail.com>
2021-04-12 14:52:49 -0400

    Primer commit de la web de prueba

---
+++ js/app.js
@@ -0,0 +1,16 @@
+document.querySelector('.menu-btn').addEventListener('click', () => 
+{
+    document.querySelector('.nav-menu').classList.toggle('show')
+});
+
+
+/* Codigo para Scroll Purete */
+
+ScrollReveal().reveal('.showcase', {delay: 500 });
+ScrollReveal().reveal('.news-cards', {delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', {delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', {delay: 500 });
+
+
+
+// Pyturn, 
